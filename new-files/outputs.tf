12c3189b9196b2db0ee26cbcd6a4c1e1de2cec1e
Restyled.io <commits@restyled.io>
2019-01-27 18:04:24 +0000

    Restyled by terraform

--- new-files/outputs.tf
+++ new-files/outputs.tf
@@ -2,7 +2,6 @@ output "ip_address" {
   value = "${azurerm_container_group.aci-example.ip_address}"
 }
 
-output "fqdn"
-{
-    value = "${azurerm_container_group.aci-example.fqdn}"
+output "fqdn" {
+  value = "${azurerm_container_group.aci-example.fqdn}"
 }
