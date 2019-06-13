b33dd426868dbe71cb08560f8edf0a5c68bf3381
Restyled.io <commits@restyled.io>
2019-06-13 13:51:57 +0000

    Restyled by elm-format

--- new-files/case.elm
+++ new-files/case.elm
@@ -1,6 +1,20 @@
-homeDirectory = "/root/files"
-eval boolean = case boolean of
-    Literal bool -> bool
-    Not b        -> not (eval b)
-    And b b_     -> eval b && eval b_
-    Or b b_      -> eval b   || eval b_
+module Main exposing (..)
+
+
+homeDirectory =
+    "/root/files"
+
+
+eval boolean =
+    case boolean of
+        Literal bool ->
+            bool
+
+        Not b ->
+            not (eval b)
+
+        And b b_ ->
+            eval b && eval b_
+
+        Or b b_ ->
+            eval b || eval b_
