a07da9e46973fadf67a0cb6adfd8e9ce231e4736
Manuel Hegner <manuel-hegner@users.noreply.github.com>
2019-07-12 11:38:53 +0200

    conform to multi file arguments

--- jdt/Formatter.java
+++ jdt/Formatter.java
@@ -1,15 +1,32 @@
+import java.io.File;
+
 import java.util.ArrayList;
+import java.util.Set;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Formatter {
+
+	private static final Set<String> PREFIXES = Set.of(
+		"./",
+		"src/main/java/",
+		"src/test/java/",
+		"src/main/resources/",
+		"src/test/resources/",
+	);
+
 	public static void main(String... args) throws Exception {
-		args[0] = Arrays
-			.stream(args[0].split(","))
-			.map(Formatter::removePrefix)
-			.collect(Collectors.joining(","));
-		
+		ArrayList<String> files = new ArrayList<>(args.length);
+		ArrayList<String> formatterArgs = new ArrayList<>(args.length);
+		for(String arg : args) {
+			if(new File(arg).exists()) {
+				files.add(removePrefix(arg));
+			}
+			else {
+				formatterArgs.add(arg);
+			}
+		}		
 		
 		List<String> call = new ArrayList<>();
 		call.addAll(Arrays.asList(
@@ -17,8 +34,8 @@ public class Formatter {
 			"net.revelc.code.formatter:formatter-maven-plugin:2.10.0:format",
 			"-Dproject.build.sourceEncoding=UTF-8"
 		));
-		call.add("-Dformatter.includes="+args[0]);
-		call.addAll(Arrays.asList(args).subList(1, args.length));
+		call.addAll(formatterArgs);
+		call.add("-Dformatter.includes="+files.stream().collect(Collectors.joining(",")));
 		
 		System.out.println("Calling "+call);
 		int exit = new ProcessBuilder(call)
@@ -32,8 +49,10 @@ public class Formatter {
 	}
 	
 	private static String removePrefix(String file) {
-		if(file.startsWith("./src/main/java/")) {
-			return file.substring(16);
+		for(String prefix : PREFIXES) {
+			if(file.startsWith(prefix)) {
+				file = file.substring(prefix.length());
+			}
 		}
 		return file;
 	}
